7914fc44290f1a4fc8b3cd2cc45e3b300c9d5163
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2022-04-17 01:24:10 +1000

    Update lint script

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,15 @@ module.exports = {
   extends: ['airbnb-base', 'airbnb-typescript/base'],
   parserOptions: {
     project: './tsconfig.json'
-  }
+  },
+  rules: {
+    'no-console': 'off',
+    'max-len': 'off',
+    'no-underscore-dangle': 'off',
+    'no-restricted-syntax': 'off',
+    'new-cap': 'off',
+    'no-bitwise': 'off',
+    'max-classes-per-file': 'off',
+    'no-param-reassign': 'off',
+  },
 };
