4e832f54518cab02bde00e0bb20fd36056faed28
Jonache Hilton <jonache.hilton@my.jcu.edu.au>
2022-04-16 22:57:35 +1000

    Add difficult logic

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -40,6 +40,37 @@ const getBlockchain = (): Block[] => blockchain;
 
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
+const getCurrentTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const BLOCK_GENERATION_INTERVAL: number = 10; // Seconds
+
+const DIFFICULTY_ADJUSTMENT_INTERVAL: number = 10; // Blocks
+
+function getDifficulty(aBlockchain: Block[]): number {
+  const latestBlock: Block = aBlockchain[aBlockchain.length - 1];
+
+  if (latestBlock.index % DIFFICULTY_ADJUSTMENT_INTERVAL === 0 && latestBlock.index !== 0) {
+    return getAdjustedDifficulty(latestBlock, aBlockchain);
+  } else {
+    return latestBlock.difficulty;
+  }
+}
+
+function getAdjustedDifficulty(latestBlock: Block, aBlockchain: Block[]) {
+  const prevAdjustmentBlock: Block = aBlockchain[aBlockchain.length - DIFFICULTY_ADJUSTMENT_INTERVAL];
+  const timeExpected: number = BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
+  const timeTaken: number = latestBlock.timestamp = prevAdjustmentBlock.timestamp;
+
+  if (timeTaken < timeExpected / 2) {
+    return prevAdjustmentBlock.difficulty + 1;
+  } else if (timeTaken > timeExpected * 2) {
+    return prevAdjustmentBlock.difficulty - 1;
+  } else {
+    return prevAdjustmentBlock.difficulty;
+  }
+
+}
+
 function calculateHash(index: number, previousHash: string, timestamp: number, data: Transaction[],
                       difficulty: number, minterBalance: number, minterAddress: string): string {
   return CryptoJS.SHA256(index + previousHash + timestamp + data + difficulty + minterBalance + minterAddress).toString();
@@ -52,6 +83,30 @@ function generateNextBlock(blockData: string): Block {
   const nextHash: string = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
 
   return new Block(nextIndex, nextHash, previousBlock.hash, nextTimestamp, blockData);
+}
+
+// Find a block to try puzzle
+function findBlock(index: number, previousHash: string, data: Transaction[], difficulty: number): Block {
+  let pastTimestamp: number = 0;
+
+  while (true) {
+    let timestamp: number = getCurrentTimestamp();
+
+    if (pastTimestamp !== timestamp) {
+      let hash: string = calculateHash(index, previousHash, timestamp, data, difficulty, getAccountBalance(), getPublicFromWallet());
+      
+      if (isBlockStakingValid(previousHash, getPublicFromWallet(), timestamp, getAccountBalance(), difficulty, index)) {
+        return new Block(index, hash, previousHash, timestamp, data, difficulty, getAccountBalance(), getPublicFromWallet());
+      }
+
+      pastTimestamp = timestamp;
+    }
+  }
+}
+
+function isTimestampValid(block: Block, previousBlock: Block): boolean {
+  return (previousBlock.timestamp - 60 < block.timestamp) 
+          && block.timestamp - 60 < getCurrentTimestamp();
 }
 
 function isBlockValid(block: Block, previousBlock: Block): boolean {
